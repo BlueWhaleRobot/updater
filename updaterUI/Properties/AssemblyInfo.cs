18b55f6cfe7428100525de59340e776f62fe4f7a
Zhang Weijia <newnewton@qq.com>
2018-05-13 20:57:04 +0800

    updater ui to dll and add nuspec file

--- updaterUI/Properties/AssemblyInfo.cs
+++ updaterUI/Properties/AssemblyInfo.cs
@@ -7,13 +7,13 @@ using System.Windows;
 // 有关程序集的一般信息由以下
 // 控制。更改这些特性值可修改
 // 与程序集关联的信息。
-[assembly: AssemblyTitle("updaterUI")]
-[assembly: AssemblyDescription("")]
+[assembly: AssemblyTitle("UpdaterUI")]
+[assembly: AssemblyDescription("A simple software update tool")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("")]
+[assembly: AssemblyCompany("Bluewhale Robot")]
 [assembly: AssemblyProduct("updaterUI")]
 [assembly: AssemblyCopyright("Copyright ©  2018")]
-[assembly: AssemblyTrademark("")]
+[assembly: AssemblyTrademark("bwbot")]
 [assembly: AssemblyCulture("")]
 
 // 将 ComVisible 设置为 false 会使此程序集中的类型
@@ -51,5 +51,5 @@ using System.Windows;
 // 可以指定所有值，也可以使用以下所示的 "*" 预置版本号和修订号
 // 方法是按如下所示使用“*”: :
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.0.0.1")]
+[assembly: AssemblyFileVersion("1.0.0.1")]
